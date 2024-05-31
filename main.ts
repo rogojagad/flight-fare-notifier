4bd3fbbe7a8653cc012453459b01729670abfef6
rogojagad <rogojagadalit@gmail.com>
2024-05-31 15:46:06 +0000

    remove dependency to browser binary

--- main.ts
+++ main.ts
@@ -51,7 +51,7 @@ bot.start();
 
 /** Cron */
 Deno.cron("Scrape and search flight according to stored params", {
-  minute: { every: 2 },
+  minute: { every: 15 },
 }, async () => {
   console.info(
     `Running scrape and search at ${luxon.DateTime.now().toISO()}`,
